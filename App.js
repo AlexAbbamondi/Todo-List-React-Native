52de217ebd488b60f0fea8887efb2a0a05a570fc
Alex Abbamondi <alex.abbamondi@gmail.com>
2021-01-01 14:13:34 -0500

    finished app

--- App.js
+++ App.js
@@ -1,43 +1,57 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from "react-native";
 import Header from "./components/Header";
 import TodoItem from "./components/TodoItem";
 import AddTodo from "./components/AddTodo";
 
 export default function App() {
   const [todos, setTodos] = useState([
-    {text: "buy coffee", key: "1"},
-    {text: "create an app", key: "2"},
-    {text: "play on the switch", key: "3"}
+    { text: "buy coffee", key: "1" },
+    { text: "create an app", key: "2" },
+    { text: "play on the switch", key: "3" },
   ]);
 
   //Delete a todo item
   const pressHandler = (key) => {
     setTodos((prevTodos) => {
-      return prevTodos.filter(todo => todo.key != key)
+      return prevTodos.filter((todo) => todo.key != key);
     });
-  }
+  };
 
-  //submit a todo item
+  //submit a todo item thats greater than 3 characters, gives alert if its too short
   const submitHandler = (text) => {
-    setTodos((prevTodos) => {
-      return [ {text: text, key: Math.random().toString()}, ...prevTodos ]
-    })
-  }
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        return [{ text: text, key: Math.random().toString() }, ...prevTodos];
+      });
+    } else {
+        Alert.alert("OOPS!", "Todos must be over 3 characters long", [
+          {text: "Understood", onPress= () => console.log("alert Closed")}
+        ])
+    }
+  };
+
   //Main layout
-    return(
+  return (
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+    }}>
       <View style={styles.container}>
         <Header />
         <View syle={styles.content}>
-        <AddTodo submitHandler={submitHandler}/>
+          <AddTodo submitHandler={submitHandler} />
           <View style={styles.list}>
-            <FlatList data={todos} renderItem={({item}) => (
-              <TodoItem item={item} pressHandler={pressHandler}/>
-            )}/>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
           </View>
         </View>
       </View>
-    ) 
+    </TouchableWithoutFeedback>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -47,9 +61,8 @@ const styles = StyleSheet.create({
   },
   content: {
     padding: 40,
-
   },
   list: {
-    marginTop: 20
-  }
-})
+    marginTop: 20,
+  },
+});
